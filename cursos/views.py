379e2b03682ed4a6d6f740fc67dd3f750ee2c3b1
Diego Carlos <diego_carlos110@hotmail.com>
2022-06-23 15:31:57 -0300

    comite inicial

---
+++ cursos/views.py
@@ -0,0 +1,21 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+from .models import Curso, Avaliacao
+from serializers import CursoSerializer, AvaliacaoSerializer
+
+class CursoAPIView(APIView):
+    '''API de cursos da geek university'''
+    def get(self, request):
+        cursos = Curso.objects.all()
+        serializer = CursoSerializer(cursos, many=True)
+
+        return Response(serializer.data)
+
+class AvaliacaoAPIView(APIView):
+    '''API de avaliação'''
+    def get(self, request):
+        avaliacoes = Avaliacao.objects.all()
+        serializer = AvaliacaoSerializer(avaliacoes, many=True)
+
+        return Response(serializer.data)
